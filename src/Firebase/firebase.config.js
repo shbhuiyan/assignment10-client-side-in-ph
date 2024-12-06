aedff2799d0017f791f1cafa3553ce59f440b064
shbhuiyan <shbhuiyan997@gmail.com>
2024-12-06 23:04:21 +0600

    added dotenv

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBzcKffablnyZWiSCkoAIKFYfecBucyLBI",
-  authDomain: "all-assignments-p-hero.firebaseapp.com",
-  projectId: "all-assignments-p-hero",
-  storageBucket: "all-assignments-p-hero.firebasestorage.app",
-  messagingSenderId: "845107112696",
-  appId: "1:845107112696:web:fa2d99543dbb029924a90d"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
