92b29bd81ead47ae467eba1d66fd462dc0992dda
shbhuiyan <shbhuiyan997@gmail.com>
2024-12-08 16:57:58 +0600

    added partners section

--- src/Components/Partners/Partners.jsx
+++ src/Components/Partners/Partners.jsx
@@ -1,8 +1,16 @@
+import Marquee from "react-fast-marquee";
 
 const Partners = () => {
     return (
-        <div>
-            partners
+        <div className="space-y-10">
+            <h1 className="text-4xl font-bold text-center"><span className="border-b-2 pb-3 border-blue-600">Our Official Partners</span></h1>
+            <Marquee className="bg-base-100 p-4 border-y ">
+                <img className="max-w-20 mr-60" src="https://i.postimg.cc/B66RCdjP/adidas.png" alt="" />
+                <img className="max-w-20 mr-60" src="https://i.postimg.cc/SN6qjbCm/Air-Jordan-Symbol.png" alt="" />
+                <img className="max-w-20 mr-60" src="https://i.postimg.cc/ryW2Y2h5/nb.png" alt="" />
+                <img className="max-w-20 mr-60" src="https://i.postimg.cc/fRW4LtMv/Nike-Logo.png" alt="" />
+                <img className="max-w-20 mr-60" src="https://i.postimg.cc/R0qj7k66/puma.jpg" alt="" />
+            </Marquee>
         </div>
     );
 };
